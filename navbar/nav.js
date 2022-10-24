9ce6058992e0c09d8974189247ab81a66ebcc18e
SHARAN-Sh <shakyasaran100@gmail.com>
2022-10-24 10:32:22 +0545

    jhg

--- navbar/nav.js
+++ navbar/nav.js
@@ -2,8 +2,8 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
+hamburger.addEventListener('click', () => {
+    //Animate Links
     navLinks.classList.toggle("open");
     links.forEach(link => {
         link.classList.toggle("fade");
@@ -12,3 +12,17 @@ hamburger.addEventListener('click', ()=>{
     //Hamburger Animation
     hamburger.classList.toggle("toggle");
 });
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("logo").style.height = "3.1rem";
+        document.getElementById("logo").style.width = "3.1rem";
+
+    } else {
+        document.getElementById("logo").style.height = "4.25rem";
+        document.getElementById("logo").style.width = "4.25rem";
+    }
+}
