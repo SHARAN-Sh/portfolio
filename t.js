66660efc95a9cff2bc38ce698d0bb579c0234f80
SHARAN-Sh <shakyasaran100@gmail.com>
2022-11-02 17:00:31 +0545

    fito

--- t.js
+++ t.js
@@ -41,6 +41,19 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
 });
 
 
+//-------Scroll navbar-----
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
 
 
 
