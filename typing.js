5c38674d55209ae26f0eaa3fc2881dd7b80f14fa
SHARAN-Sh <shakyasaran100@gmail.com>
2022-11-02 11:46:40 +0545

    footerleft

--- typing.js
+++ typing.js
@@ -39,3 +39,12 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 200);
 });
+
+
+
+
+
+
+
+
+
